ccfb8ffa731d7292a21840aab0df3c51dccf8047
Prashanth Dsouza <prashanthdsouza17@gmail.com>
2022-07-14 14:27:19 +0530

    updated files or some

--- src/components/Expenses/NewExpense/ExpenseForm.js
+++ src/components/Expenses/NewExpense/ExpenseForm.js
@@ -3,37 +3,40 @@ import React, {useState} from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState('');
-  // const [enteredAmount, setEnteredAmount] = useState('');
-  // const [enteredDate, setEnteredDate] = useState('');
-  const [userInput, setUserInput] = useState({
-    EnteredTitle:'',
-    EnteredAmount:'',
-    EnteredDate:''
-  });
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+  // const [userInput, setUserInput] = useState({
+  //   EnteredTitle:'',
+  //   EnteredAmount:'',
+  //   EnteredDate:''
+  // });
 
   const titleChangeHandler = (event) => {
-    // setEnteredTitle(event.target.value);
-    setUserInput({
-      ...userInput,
-      EnteredTitle: event.target.value
-    })
+    setEnteredTitle(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   EnteredTitle: event.target.value
+    // })
+    // setUserInput ((prevState) => {
+    //   return { ...prevState, EnteredTitle: Event.target.value};
+    // })
   };
 
   const amountChangeHandler = event =>{
-    // setEnteredAmount(event.target.value);
-    setUserInput({
-      ...userInput,
-      EnteredAmount: event.target.value
-    })
+    setEnteredAmount(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   EnteredAmount: event.target.value
+    // })
   };
 
   const dateChangeHandler = event => {
-    // setEnteredDate(event.target.value);
-    setUserInput({
-      ...userInput,
-      EnteredDate: event.target.value
-    })
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   EnteredDate: event.target.value
+    // })
   };
 
   return (
